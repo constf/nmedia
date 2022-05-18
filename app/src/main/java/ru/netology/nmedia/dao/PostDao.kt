01eaa3b09d5d1e3711f4bfd3159f9ea22ab170af
constf <k.faraonov@normais.ru>
2022-05-18 15:57:10 +0300

    Room initial commit

--- app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
+++ app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
@@ -1,13 +1,40 @@
 package ru.netology.nmedia.dao
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 import ru.netology.nmedia.dto.Post
 
+@Dao
 interface PostDao {
-    fun getAll(): List<Post>
-    fun save(post: Post): Post
+
+    @Query("SELECT * FROM posts ORDER BY id ASC")
+    fun getAll(): Flow<List<PostEntity>>
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(post: PostEntity)
+
+
+    @Query("DELETE FROM posts WHERE id = :id")
     fun removeById(id: Long)
+
+
+    @Query("""UPDATE posts SET
+                numLikes = numLikes + CASE WHEN likedByMe THEN -1 ELSE 1 END,
+                likedByMe = CASE WHEN likedByMe THEN 0 ELSE 1 END
+                WHERE  id = :id""")
     fun likeById(id: Long)
+
+
+    @Query("UPDATE posts SET numShares = numShares + 1 WHERE  id = :id")
     fun shareById(id: Long)
+
+
+    @Query("UPDATE posts SET numViews = numViews + 1 WHERE id = :id")
     fun viewById(id: Long)
-    fun getById(id: Long): Post
+
+
+    @Query("SELECT * FROM posts WHERE id = :id LIMIT 1")
+    fun getById(id: Long): PostEntity
 }
